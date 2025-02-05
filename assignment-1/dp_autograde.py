4821c231249d427f71fc2305b528dc4f58aba0b0
matthias.hagenauer <matthias.hagenauer@student.uva.nl>
2025-02-05 16:29:44 +0100

    some progress

--- assignment-1/dp_autograde.py
+++ assignment-1/dp_autograde.py
@@ -21,146 +21,6 @@ def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
     # Start with a all 0 value function
     V = np.zeros(env.nS)
     # YOUR CODE HERE
-    delta = np.inf
-    while delta > theta:
-        for state in range(env.nS):
-            v = V[state].copy()
-            sum_policy_state_action = 0
-            for action in range(env.nA):
-                sum_state_action = 0
-                for prob, next_state, reward, done in env.P[state][action]:
-                    sum_state_action += * prob * (reward + discount_factor * V[next_state])
-                sum_policy_state_action += policy[state][action] * sum_state_action
-            V[state] = sum_policy_state_action
-            delta = max([delta, np.sum(abs(v - V[state]))])
-        
-    return np.array(V)
-
-def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
-    """
-    Evaluate a policy given an environment and a full description of the environment's dynamics.
-
-    Args:
-        policy: [S, A] shaped matrix representing the policy.
-        env: OpenAI env. env.P represents the transition probabilities of the environment.
-            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-            env.nS is a number of states in the environment.
-            env.nA is a number of actions in the environment.
-        theta: We stop evaluation once our value function change is less than theta for all states.
-
-        discount_factor: Gamma discount factor.
-
-    Returns:
-        Vector of length env.nS representing the value function.
-    """
-    # Start with a all 0 value function
-    V = np.zeros(env.nS)
-    # YOUR CODE HERE
-    delta = np.inf
-    while delta > theta:
-        for state in range(env.nS):
-            v = V[state].copy()
-            sum_policy_state_action = 0
-            for action in range(env.nA):
-                sum_state_action = 0
-                for prob, next_state, reward, done in env.P[state][action]:
-                    sum_state_action += prob * (reward + discount_factor * V[next_state])
-                sum_policy_state_action += policy[state][action] * sum_state_action
-            V[state] = sum_policy_state_action
-            delta = max([delta, np.sum(abs(v - V[state]))])
-        
-    return np.array(V)
-
-def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
-    """
-    Evaluate a policy given an environment and a full description of the environment's dynamics.
-
-    Args:
-        policy: [S, A] shaped matrix representing the policy.
-        env: OpenAI env. env.P represents the transition probabilities of the environment.
-            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-            env.nS is a number of states in the environment.
-            env.nA is a number of actions in the environment.
-        theta: We stop evaluation once our value function change is less than theta for all states.
-
-        discount_factor: Gamma discount factor.
-
-    Returns:
-        Vector of length env.nS representing the value function.
-    """
-    # Start with a all 0 value function
-    V = np.zeros(env.nS)
-    # YOUR CODE HERE
-    delta = theta * 2
-    while delta > theta:
-        for state in range(env.nS):
-            v = V[state].copy()
-            sum_policy_state_action = 0
-            for action in range(env.nA):
-                sum_state_action = 0
-                for prob, next_state, reward, done in env.P[state][action]:
-                    sum_state_action += prob * (reward + discount_factor * V[next_state])
-                sum_policy_state_action += policy[state][action] * sum_state_action
-            V[state] = sum_policy_state_action
-            delta = max([delta, np.sum(abs(v - V[state]))])
-        
-    return np.array(V)
-
-def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
-    """
-    Evaluate a policy given an environment and a full description of the environment's dynamics.
-
-    Args:
-        policy: [S, A] shaped matrix representing the policy.
-        env: OpenAI env. env.P represents the transition probabilities of the environment.
-            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-            env.nS is a number of states in the environment.
-            env.nA is a number of actions in the environment.
-        theta: We stop evaluation once our value function change is less than theta for all states.
-
-        discount_factor: Gamma discount factor.
-
-    Returns:
-        Vector of length env.nS representing the value function.
-    """
-    # Start with a all 0 value function
-    V = np.zeros(env.nS)
-    # YOUR CODE HERE
-    delta = theta * 1.0000001
-    while delta > theta:
-        for state in range(env.nS):
-            v = V[state].copy()
-            sum_policy_state_action = 0
-            for action in range(env.nA):
-                sum_state_action = 0
-                for prob, next_state, reward, done in env.P[state][action]:
-                    sum_state_action += prob * (reward + discount_factor * V[next_state])
-                sum_policy_state_action += policy[state][action] * sum_state_action
-            V[state] = sum_policy_state_action
-            delta = max([delta, np.sum(abs(v - V[state]))])
-        
-    return np.array(V)
-
-def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
-    """
-    Evaluate a policy given an environment and a full description of the environment's dynamics.
-
-    Args:
-        policy: [S, A] shaped matrix representing the policy.
-        env: OpenAI env. env.P represents the transition probabilities of the environment.
-            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-            env.nS is a number of states in the environment.
-            env.nA is a number of actions in the environment.
-        theta: We stop evaluation once our value function change is less than theta for all states.
-
-        discount_factor: Gamma discount factor.
-
-    Returns:
-        Vector of length env.nS representing the value function.
-    """
-    # Start with a all 0 value function
-    V = np.zeros(env.nS)
-    # YOUR CODE HERE
     while True:
         delta = 0
         for state in range(env.nS):
@@ -178,3 +38,108 @@ def policy_eval_v(policy, env, discount_factor=1.0, theta=0.00001):
             break
         
     return np.array(V)
+
+def policy_iter_v(env, policy_eval_v=policy_eval_v, discount_factor=1.0):
+    """
+    Policy Iteration Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+
+    Args:
+        env: The OpenAI envrionment.
+        policy_eval_v: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: gamma discount factor.
+
+    Returns:
+        A tuple (policy, V).
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+
+    """
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+    # YOUR CODE HERE
+    policy_stable = False
+    while not policy_stable:
+        policy_stable = true
+        V = policy_eval_v(policy, env, discount_factor=discount_factor)
+        for state in range(env.nS):
+            old_action_distribution = policy[state].copy()
+            for action in range(env.nA):
+                for prob, next_state, reward, done in env.P[state][action]:
+                     policy[state][action] = prob * (reward + discount_factor * V[next_state])
+                if old_action_distribution != policy[state]:
+                    policy_stable = False
+
+    return policy, V
+
+def policy_iter_v(env, policy_eval_v=policy_eval_v, discount_factor=1.0):
+    """
+    Policy Iteration Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+
+    Args:
+        env: The OpenAI envrionment.
+        policy_eval_v: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: gamma discount factor.
+
+    Returns:
+        A tuple (policy, V).
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+
+    """
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+    # YOUR CODE HERE
+    policy_stable = False
+    while not policy_stable:
+        policy_stable = True
+        V = policy_eval_v(policy, env, discount_factor=discount_factor)
+        for state in range(env.nS):
+            old_action_distribution = policy[state].copy()
+            for action in range(env.nA):
+                for prob, next_state, reward, done in env.P[state][action]:
+                     policy[state][action] = prob * (reward + discount_factor * V[next_state])
+                if old_action_distribution != policy[state]:
+                    policy_stable = False
+
+    return policy, V
+
+def policy_iter_v(env, policy_eval_v=policy_eval_v, discount_factor=1.0):
+    """
+    Policy Iteration Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+
+    Args:
+        env: The OpenAI envrionment.
+        policy_eval_v: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: gamma discount factor.
+
+    Returns:
+        A tuple (policy, V).
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+
+    """
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+    # YOUR CODE HERE
+    policy_stable = False
+    while not policy_stable:
+        policy_stable = True
+        V = policy_eval_v(policy, env, discount_factor=discount_factor)
+        for state in range(env.nS):
+            old_action_distribution = policy[state].copy()
+            for action in range(env.nA):
+                for prob, next_state, reward, done in env.P[state][action]:
+                     policy[state][action] = prob * (reward + discount_factor * V[next_state])
+                if all(old_action_distribution != policy[state]):
+                    policy_stable = False
+
+    return policy, V
